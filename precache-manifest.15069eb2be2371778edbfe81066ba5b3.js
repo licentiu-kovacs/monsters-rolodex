d29f197b4f95c967e8f28a0f85edad307e08d13d
Licentiu Kovacs <MY_NAME@example.com>
2020-07-22 18:23:23 +0300

    Updates

---
+++ precache-manifest.15069eb2be2371778edbfe81066ba5b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b764faf11f70ceb862b3366f3daafb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "93828a5fe016f0e0e070",
+    "url": "/monsters-rolodex/static/css/main.11618a6c.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93828a5fe016f0e0e070",
+    "url": "/monsters-rolodex/static/js/main.2d719b29.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
